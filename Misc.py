c2209abd2e021b38f5cbea05addd39f9eb830a24
Yatrik Sachdeva <yatriksachdeva@gmail.com>
2015-09-19 16:42:10 +0530

    Added a misc script file. This file is place to test/learn new things in Python.

---
+++ Misc.py
@@ -0,0 +1,25 @@
+# Place to test and verify new things
+
+# Keyboard input
+# There is difference between inut and raw_input
+# function.
+input = input('Enter a number:')
+print input
+
+input = raw_input('Enter a string:')
+print input
+
+
+def sum(a, b):
+    return a + b
+
+
+def another_sum_func(a, b):
+    a + b
+
+
+sum(2, 3)
+print 'sum function called !!'
+# Following resulted in Traceback:
+# TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
+# print 'another_sum_func called', another_sum_func(1, 2) + 7
