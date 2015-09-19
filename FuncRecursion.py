bb3c407d1fb3e3678c449a61136a5158a63c6df4
Yatrik Sachdeva <yatriksachdeva@gmail.com>
2015-09-19 13:45:25 +0530

    Added exercise file related to functions and recursion

---
+++ FuncRecursion.py
@@ -0,0 +1,19 @@
+# Functions and Recursion
+
+
+def count_down(n):
+    if n == 0:
+        print "Blast Off !!"
+    else:
+        print n
+        count_down(n - 1)
+
+
+def print_lines(n):
+    if n > 0:
+        print
+        print_lines(n - 1)
+
+
+print_lines(5)
+count_down(5)
