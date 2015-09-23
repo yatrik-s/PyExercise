586c656f6c832294b6d104ebc7c1109eb010c8ca
Yatrik Sachdeva <yatriksachdeva@gmail.com>
2015-09-23 23:03:26 +0530

    Tested while statement with else block.

--- Misc.py
+++ Misc.py
@@ -23,3 +23,11 @@ print 'sum function called !!'
 # Following resulted in Traceback:
 # TypeError: unsupported operand type(s) for +: 'NoneType' and 'int'
 # print 'another_sum_func called', another_sum_func(1, 2) + 7
+
+# Testing while with else block
+number = 0
+while number < 5:
+    print "number = {0}".format(number)
+    number += 1
+else:
+    print "number {0} is no longer less than 5".format(number)
