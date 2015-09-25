a59abb19711dfcb6bee3b96b90cafc6d18142644
Yatrik Sachdeva <Yatrik.Sachdeva@safenet-inc.com>
2015-09-25 20:19:15 +0530

    Added TCP server.

---
+++ Networking/TCPServer.py
@@ -0,0 +1,25 @@
+from socket import *
+from time import ctime
+
+HOST = ''
+PORT = 21567
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+tcpSocket = socket(AF_INET, SOCK_STREAM)
+tcpSocket.bind(ADDR)
+tcpSocket.listen(5)
+
+while True:
+    print 'Waiting for connection...'
+    tcpCSocket, addr = tcpSocket.accept()
+    print '...connected from:', addr
+
+    while True:
+        data = tcpCSocket.recv(BUFSIZE)
+        if not data:
+            break
+        tcpCSocket.send('[%s]:%s' % (ctime(), data))
+
+    tcpCSocket.close()
+tcpSocket.close()
